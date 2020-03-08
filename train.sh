907e7b677245cc8f5649c65d4dd4d4416e423f65
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-03-08 00:16:35 +0000

    added training script

---
+++ train.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+python -u examples/train_model.py \
+	-t opensubtitles \
+	-mf parlai_internal/backward.ckpt \
+	-bs 64 \
+	-m transformer/generator \
+	--ffn-size 512 \
+	-esz 512 \
+	-vtim 21600 \
+	-sval True \
+	-opt adam \
+	-lr 0.1 \
+	-df data/OpenSubtitles2018/opensubtitles.dict \
+	--n-heads 4 \
+	--dropout 0.1 \
+	--attention-dropout 0.1 \
+	-eps 10 \
+	-ttim 86400
