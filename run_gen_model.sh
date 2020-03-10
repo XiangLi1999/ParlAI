5c00b7c8ccffc2c6054bad6db2024eb76c71fbf2
Alexandra DeLucia <aadelucia@jhu.edu>
2020-03-10 17:05:18 -0400

    added training scripts

--- run_gen_model.sh
+++ run_gen_model.sh
@@ -2,8 +2,7 @@
 
 python -m parlai.scripts.interactive \
 	-mf parlai_internal/forward.ckpt.checkpoint \
-	--model-file-backward parlai_internal/backward.ckpt.checkpoint \
-	--model transformer/generatorMMI \
+	--model transformer/generator \
 	--no-cuda \
 	--beam-size 8 \
 	--inference "beam"
