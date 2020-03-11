527bbb1c15c2a3d6d19467b659501bb2e8675e25
Alexandra DeLucia <aadelucia@jhu.edu>
2020-03-11 15:14:09 -0400

    updated permissions

--- run_mmi_model.sh
+++ run_mmi_model.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+source /home/aadelucia/miniconda3/bin/activate
+conda activate dialogue
+
+PROJECT_HOME=/home/aadelucia/files/course_projects/discourse-hw4/ParlAI
 
 python -m parlai.scripts.interactive \
-	-mf parlai_internal/forward.ckpt.checkpoint \
-	--model-file-backward parlai_internal/backward.ckpt.checkpoint \
+	-mf $PROJECT_HOME/parlai_internal/forward.ckpt.checkpoint \
+	--model-file-backward $PROJECT_HOME/parlai_internal/backward.ckpt.checkpoint \
 	--model transformer/generatorMMI \
 	--no-cuda \
 	--beam-size 8 \
