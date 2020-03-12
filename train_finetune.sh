4ec9ba18e4342c0dbaa000906f3bfb3d079d423f
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-03-12 03:55:15 +0000

    updated train time

--- train_finetune.sh
+++ train_finetune.sh
@@ -5,17 +5,18 @@ python -m parlai.scripts.multiprocessing_train \
 	-mf parlai_internal/forward_finetune.ckpt \
 	-bs 16 \
 	-m transformer/generator \
-	-stim 21600 \
+	-stim 10800 \
 	-sval True \
 	-opt sgd \
 	-lr 0.1 \
 	--embedding-type fasttext_cc \
 	--beam-size 5 \
+	--inference beam \
 	-df data/OpenSubtitles2018/opensubtitles.dict \
 	--dropout 0.1 \
 	--attention-dropout 0.1 \
 	-eps 10 \
-	-ttim 43200 \
+	-ttim 129600 \
 	-lfc True \
 	--truncate 1024
 
