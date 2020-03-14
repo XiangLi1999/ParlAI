0ea89bca1c875e1fbe9f4b4a82b576fe29a7df90
Alexandra DeLucia <aadelucia@jhu.edu>
2020-03-14 13:49:26 -0400

    reference local worlds.py

--- mturk_evaluation/run.py
+++ mturk_evaluation/run.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 from parlai.core.params import ParlaiParser
-from parlai.mturk.tasks.model_evaluator.worlds import (
+from worlds import (
     ModelEvaluatorWorld,
     ModelEvaluatorOnboardWorld,
 )
